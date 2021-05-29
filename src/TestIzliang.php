3f156f99c34d4ff7c7c1f6384b6c889100942212
izliang <izliang@qq.com>
2021-05-29 16:59:58 +0800

    增加方法，测试速度

--- src/TestIzliang.php
+++ src/TestIzliang.php
@@ -7,4 +7,10 @@ class TestIzliang
     public static function test(){
         echo "composer success";
     }
+
+    public static function test1(){
+        $arrTest = [];
+        $arrTest['data'] = "666";
+        var_dump($arrTest);
+    }
 }
